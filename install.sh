e7a7aac3e3e46b10715d0732e14aae609dc629e9
Pinger <pinger@gmail.com>
2020-02-20 18:55:17 +0000

    Updated

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y stow python-pip keychain vim gnome-tweaks tilix curl
+sudo apt install -y stow python-pip keychain vim curl
 pip install powerline-shell
 stow bash
 stow ssh
@@ -10,4 +10,4 @@ stow powerline
 echo "if [ -f ~/.bash_profile_custom ]; then . ~/.bash_profile_custom; fi" >> ~/.bashrc
 mkdir ~/.fonts
 curl -s -k -o ~/.fonts/Hack-Regular.ttf https://github.com/powerline/fonts/raw/master/Hack/Hack-Regular.ttf
-dconf load /com/gexperts/Tilix/ < tilix/tilix.dconf
+#dconf load /com/gexperts/Tilix/ < tilix/tilix.dconf
