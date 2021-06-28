13ba4e3ee39b43875ae51e88041e20dcb3d8cc51
Paul Seymour <paul.seymour@arcadia.com>
2021-06-28 16:14:36 -0400

    Updates to zsh, brewfile and git config

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y stow python3-pip keychain vim curl fonts-hack-ttf jq rbenv zsh fonts-powerline tmux
+sudo apt install -y stow python3-pip keychain vim curl fonts-hack-ttf jq rbenv zsh fonts-powerline tmux libpq-dev
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew bundle --file=brew/Brewfile
-pip3 install powerline-shell
+#pip3 install powerline-shell
 stow bash
 stow ssh
 stow tmux
@@ -21,4 +21,4 @@ dconf load /com/gexperts/Tilix/ < tilix/tilix.dconf
 #sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 #sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 #sudo chmod +x /usr/local/bin/*
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
+#wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
