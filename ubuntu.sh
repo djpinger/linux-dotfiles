7de0bfb84c4deb117daf5d2db0ca0cb6ee89ff2e
Paul Seymour <paul.seymour@arcadia.com>
2022-01-28 21:32:46 -0500

    Tweaks

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y stow python3-pip keychain vim curl fonts-hack-ttf jq rbenv zsh fonts-powerline tmux libpq-dev
+sudo apt install -y stow python3-pip keychain vim curl jq zsh tmux libpq-dev
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew bundle --file=brew/Brewfile
@@ -17,13 +17,5 @@ stow vim
 stow powerline
 stow git
 stow zsh
-echo "if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi" >> ~/.bashrc
+#echo "if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi" >> ~/.bashrc
 dconf load /com/gexperts/Tilix/ < tilix/tilix.dconf
-mkdir ~/gems
-#curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-#sudo mv kubectl /usr/local/bin/
-#sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-#sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-#sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-#sudo chmod +x /usr/local/bin/*
-#wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
