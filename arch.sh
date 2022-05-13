66b634465d2c4dafc71f2792a72056eec4cbe030
Paul Seymour <paul.seymour@arcadia.com>
2022-05-13 17:58:43 -0400

    More tweaks

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --noconfirm zsh-theme-powerlevel10k-git 1password slack-desktop discord vim stow aws-cli aws-vault k9s kubectx eksctl kubectl keychain exa ttf-meslo-nerd-font-powerlevel10k helm exa asdf-vm
+yay -S --noconfirm zsh-theme-powerlevel10k-git 1password slack-desktop discord vim stow aws-cli aws-vault k9s kubectx eksctl kubectl keychain exa ttf-meslo-nerd-font-powerlevel10k helm exa asdf-vm noto-fonts-emoji noto-color-emoji-fontconfig
 if [ ! -d ~/.config ]
 then
 	mkdir ~/.config
