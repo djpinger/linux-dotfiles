d1ce9dc6659af5aee49fd69c7b5511d79015b2da
Paul Seymour <pauls@picnicscore.net>
2023-04-05 16:19:21 -0400

    Updates

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y stow python3-pip keychain vim curl jq zsh tmux libpq-dev
+sudo apt install -y stow python3-pip keychain vim curl jq zsh tmux libpq-dev apt-transport-https terminator
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
+sudo apt-get update && sudo apt-get install google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew bundle --file=brew/Brewfile
