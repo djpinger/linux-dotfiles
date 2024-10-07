a331efcdd28cfb321cb65c9473d6a78cd31830f8
Paul Seymour <pauls@getpicnic.com>
2024-10-07 16:05:50 -0400

    Tweaks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGM Nerd Font Mono")
-config.font_size = 15
+config.font_size = 16
 config.color_scheme = 'Catppuccin Mocha'
 
 config.enable_tab_bar = false
